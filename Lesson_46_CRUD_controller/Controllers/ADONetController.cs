4a49b73569d500b90026edcd320ec603ec887e74
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-02-20 15:38:03 +0500

    create Put function

--- Lesson_46_CRUD_controller/Controllers/ADONetController.cs
+++ Lesson_46_CRUD_controller/Controllers/ADONetController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using System.Globalization;
 
 namespace Lesson_46_CRUD_controller.Controllers
 {
@@ -9,19 +10,16 @@ namespace Lesson_46_CRUD_controller.Controllers
     [ApiController]
     public class ADONetController : ControllerBase
     {
-        private readonly string CONNECTION_STRING;
-        public ADONetController()
-        {
-            CONNECTION_STRING = "Host=localhost;Port=5432;Database=HTTP;User Id=postgres;Password=abdullayev;";
-        }
+        private readonly string CONNECTION_STRING = "Host=localhost;Port=5432;Database=HTTP;User Id=postgres;Password=abdullayev;";
+
         [HttpGet]
-        public List<string> Get()
+        public List<Products> Get()
         {
-            using(NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING))
+            using (NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING))
             {
                 string query = "select * from products";
                 connection.Open();
-                NpgsqlCommand npgsqlCommand = connection.CreateCommand();
+                NpgsqlCommand npgsqlCommand = new NpgsqlCommand(query, connection);
                 var res = npgsqlCommand.ExecuteReader();
                 List<Products> products = new List<Products>();
                 while (res.Read())
@@ -33,26 +31,41 @@ namespace Lesson_46_CRUD_controller.Controllers
                         Description = (string)res[2],
                         PhotoPath = (string)res[3]
                     });
-
                 }
                 return products;
             }
         }
         [HttpPost]
-        public string Post(string name,string description,string PhotoPath) 
+        public string Post(string name, string description, string PhotoPath)
         {
             using (NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING))
             {
-                string query = $"insert into products(Name,Description,Photopath)" +
-                    $"values(@Name,@Description,@Photopath)";
+                string query = $"insert into products(name,description,photopath) values(@name,@description,@photopath)";
                 connection.Open();
-                NpgsqlCommand command = new NpgsqlCommand(query,connection);
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
                 command.Parameters.AddWithValue("name", name);
                 command.Parameters.AddWithValue("description", description);
-                command.Parameters.AddWithValue ("PhotoPath", PhotoPath);
+                command.Parameters.AddWithValue("PhotoPath", PhotoPath);
                 int status = command.ExecuteNonQuery();
 
                 return $"Post status [=> {status}";
+            }
+        }
+        [HttpPut]
+
+        public string Put(int id,string name,string description,string photopath)
+        {
+            using(NpgsqlConnection connection = new NpgsqlConnection(CONNECTION_STRING))
+            {
+                string query = $"update products set name = @name,description = @description,photopath = @photopath where id = @id";
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand(query , connection);
+                command.Parameters.AddWithValue("id", id);
+                command.Parameters.AddWithValue("name", name);
+                command.Parameters.AddWithValue("description", description);
+                command.Parameters.AddWithValue("photopath", photopath);
+                int status = command.ExecuteNonQuery();
+                return $"Put status [->{status}";
             }
         }
     }
